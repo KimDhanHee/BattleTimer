b71461785837a2dbcb3da18c592850681eba8e6e
kimdanhee <sgoiboy@gmail.com>
2023-01-07 12:08:34 +0900

    color 추가

--- app/src/main/java/pony/tothemoon/battletimer/ui/theme/Color.kt
+++ app/src/main/java/pony/tothemoon/battletimer/ui/theme/Color.kt
@@ -6,3 +6,6 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val White900 = Color(0xFFE2E3E4)
+val Red900 = Color(0xFFFF7D33)
