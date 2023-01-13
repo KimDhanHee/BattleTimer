811131f9d0bf2bd6aeee9848ed6319d7ba9df619
kimdanhee <sgoiboy@gmail.com>
2023-01-13 17:06:21 +0900

    timerviewmodel

--- app/src/main/java/pony/tothemoon/battletimer/viewmodel/TimerViewModel.kt
+++ app/src/main/java/pony/tothemoon/battletimer/viewmodel/TimerViewModel.kt
@@ -6,36 +6,51 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import pony.tothemoon.battletimer.model.TimerInfo
 
 class TimerViewModel(timerInfo: TimerInfo) : ViewModel() {
-  var timerUiState: TimerUiState by mutableStateOf(TimerUiState.Paused(timerInfo.time))
+  var battleTimer by mutableStateOf(TimerInfo(title = "익명의 코뿔소", time = timerInfo.time))
     private set
 
-  private var timerJob: Job? = null
+  private suspend fun startBattle() {
+    while (timerUiState.time > 0) {
+      delay(1000)
 
-  fun start() {
-    timerJob?.cancel()
-    timerJob = viewModelScope.launch {
-      while (true) {
-        timerUiState = TimerUiState.Running(timerUiState.time - 100)
-        delay(100)
-      }
+      timerUiState = TimerUiState.Running(timerUiState.time - TimerInfo.SECONDS_UNIT)
+
+      battleTimer = battleTimer.copy(time = battleTimer.time - TimerInfo.SECONDS_UNIT)
     }
   }
 
-  fun pause() {
-    timerJob?.cancel()
-    timerUiState = TimerUiState.Paused(timerUiState.time)
+  var timerUiState: TimerUiState by mutableStateOf(TimerUiState.Idle(timerInfo.time))
+    private set
+
+  fun start() {
+    viewModelScope.launch {
+      if (timerUiState is TimerUiState.Idle) {
+        timerUiState = TimerUiState.Loading(timerUiState.time, "")
+
+        delay(2000)
+
+        repeat(3) {
+          timerUiState = TimerUiState.Ready(timerUiState.time, 3 - it)
+          delay(1000)
+        }
+
+        startBattle()
+      }
+    }
   }
 }
 
 sealed class TimerUiState {
+  data class Idle(override val time: Long) : TimerUiState()
+  data class Loading(override val time: Long, val text: String) : TimerUiState()
+  data class Ready(override val time: Long, val countdown: Int) : TimerUiState()
   data class Running(override val time: Long) : TimerUiState()
-  data class Paused(override val time: Long) : TimerUiState()
+  data class Finish(override val time: Long) : TimerUiState()
 
   abstract val time: Long
 }
