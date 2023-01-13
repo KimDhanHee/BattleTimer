f8d7c19a7dc72d7b186e9a1b56496d84ad912365
kimdanhee <sgoiboy@gmail.com>
2023-01-13 17:18:29 +0900

    timer preset 수정

--- app/src/main/java/pony/tothemoon/battletimer/viewmodel/TimerListViewModel.kt
+++ app/src/main/java/pony/tothemoon/battletimer/viewmodel/TimerListViewModel.kt
@@ -11,10 +11,13 @@ class TimerListViewModel : ViewModel() {
 
   init {
     _timerListFlow.value = listOf(
-      TimerInfo(title = "Timer1", time = 5 * TimerInfo.MINUTE_UNIT),
-      TimerInfo(title = "Timer2", time = 10 * TimerInfo.MINUTE_UNIT),
-      TimerInfo(title = "Timer3", time = 15 * TimerInfo.MINUTE_UNIT),
-      TimerInfo(title = "Timer4", time = 20 * TimerInfo.MINUTE_UNIT)
+      TimerInfo(title = "3분 타이머", time = 3 * TimerInfo.MINUTE_UNIT),
+      TimerInfo(title = "5분 타이머", time = 5 * TimerInfo.MINUTE_UNIT),
+      TimerInfo(title = "10분 타이머", time = 10 * TimerInfo.MINUTE_UNIT),
+      TimerInfo(title = "15분 타이머", time = 15 * TimerInfo.MINUTE_UNIT),
+      TimerInfo(title = "20분 타이머", time = 20 * TimerInfo.MINUTE_UNIT),
+      TimerInfo(title = "30분 타이머", time = 30 * TimerInfo.MINUTE_UNIT),
+      TimerInfo(title = "60분 타이머", time = 30 * TimerInfo.MINUTE_UNIT)
     )
   }
 }
