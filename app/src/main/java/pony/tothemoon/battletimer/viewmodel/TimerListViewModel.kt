fa6937a0196ee696554e62187ac6575f528ba32d
kimdanhee <sgoiboy@gmail.com>
2023-01-14 11:37:35 +0900

    배틀 타이머 조작 이동

--- app/src/main/java/pony/tothemoon/battletimer/viewmodel/TimerListViewModel.kt
+++ app/src/main/java/pony/tothemoon/battletimer/viewmodel/TimerListViewModel.kt
@@ -1,11 +1,42 @@
 package pony.tothemoon.battletimer.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import pony.tothemoon.battletimer.model.TimerInfo
 
 class TimerListViewModel : ViewModel() {
+  var battleTimer by mutableStateOf(TimerInfo(title = "익명의 코뿔소", time = 0))
+    private set
+
+  private var timerJob: Job? = null
+
+  init {
+    startBattleTimer()
+  }
+
+  fun refreshBattleTimer() {
+    battleTimer = battleTimer.copy(time = 0)
+    startBattleTimer()
+  }
+
+  private fun startBattleTimer() {
+    timerJob?.cancel()
+    timerJob = viewModelScope.launch {
+      while (true) {
+        delay(TimerInfo.SECONDS_UNIT)
+        battleTimer = battleTimer.copy(time = battleTimer.time + TimerInfo.SECONDS_UNIT)
+      }
+    }
+  }
+
   private val _timerListFlow = MutableStateFlow(emptyList<TimerInfo>())
   val timerListFlow: StateFlow<List<TimerInfo>> = _timerListFlow
 
