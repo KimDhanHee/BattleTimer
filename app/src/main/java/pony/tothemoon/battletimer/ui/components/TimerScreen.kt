d9af21868b17843ba77b8e38baceeded3ac14dce
kimdanhee <sgoiboy@gmail.com>
2023-01-14 11:46:33 +0900

    미사용 import

--- app/src/main/java/pony/tothemoon/battletimer/ui/components/TimerScreen.kt
+++ app/src/main/java/pony/tothemoon/battletimer/ui/components/TimerScreen.kt
@@ -23,7 +23,6 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
