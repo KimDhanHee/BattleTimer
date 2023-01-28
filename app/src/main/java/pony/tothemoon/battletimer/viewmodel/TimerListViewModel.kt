5ca04f0eb41151ba49609a14c05fada4a5d31e75
kimdanhee <sgoiboy@gmail.com>
2023-01-28 10:43:25 +0900

    preset 타이머 string 적용

--- app/src/main/java/pony/tothemoon/battletimer/viewmodel/TimerListViewModel.kt
+++ app/src/main/java/pony/tothemoon/battletimer/viewmodel/TimerListViewModel.kt
@@ -7,10 +7,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import pony.tothemoon.battletimer.R
 import pony.tothemoon.battletimer.model.TimerInfo
+import pony.tothemoon.battletimer.utils.AndroidUtils
 
 class TimerListViewModel : ViewModel() {
   var battleTimer by mutableStateOf(TimerInfo(title = "익명의 코뿔소", time = 0))
@@ -37,18 +37,12 @@ class TimerListViewModel : ViewModel() {
     }
   }
 
-  private val _timerListFlow = MutableStateFlow(emptyList<TimerInfo>())
-  val timerListFlow: StateFlow<List<TimerInfo>> = _timerListFlow
-
-  init {
-    _timerListFlow.value = listOf(
-      TimerInfo(title = "3분 타이머", time = 3 * TimerInfo.MINUTE_UNIT),
-      TimerInfo(title = "5분 타이머", time = 5 * TimerInfo.MINUTE_UNIT),
-      TimerInfo(title = "10분 타이머", time = 10 * TimerInfo.MINUTE_UNIT),
-      TimerInfo(title = "15분 타이머", time = 15 * TimerInfo.MINUTE_UNIT),
-      TimerInfo(title = "20분 타이머", time = 20 * TimerInfo.MINUTE_UNIT),
-      TimerInfo(title = "30분 타이머", time = 30 * TimerInfo.MINUTE_UNIT),
-      TimerInfo(title = "60분 타이머", time = 60 * TimerInfo.MINUTE_UNIT)
-    )
-  }
+  val presetTimers = arrayOf(
+    TimerInfo(title = AndroidUtils.string(R.string.minute_3_timer), time = 3 * TimerInfo.MINUTE_UNIT),
+    TimerInfo(title = AndroidUtils.string(R.string.minute_5_timer), time = 5 * TimerInfo.MINUTE_UNIT),
+    TimerInfo(title = AndroidUtils.string(R.string.minute_10_timer), time = 10 * TimerInfo.MINUTE_UNIT),
+    TimerInfo(title = AndroidUtils.string(R.string.minute_15_timer), time = 15 * TimerInfo.MINUTE_UNIT),
+    TimerInfo(title = AndroidUtils.string(R.string.minute_25_timer), time = 25 * TimerInfo.MINUTE_UNIT),
+    TimerInfo(title = AndroidUtils.string(R.string.minute_50_timer), time = 50 * TimerInfo.MINUTE_UNIT),
+  )
 }
