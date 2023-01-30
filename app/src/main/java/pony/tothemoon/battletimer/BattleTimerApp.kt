ca50d73f4076f572529108403962149375f09255
kimdanhee <sgoiboy@gmail.com>
2023-01-30 21:49:23 +0900

    admob 추가

--- app/src/main/java/pony/tothemoon/battletimer/BattleTimerApp.kt
+++ app/src/main/java/pony/tothemoon/battletimer/BattleTimerApp.kt
@@ -2,6 +2,7 @@ package pony.tothemoon.battletimer
 
 import android.app.Application
 import android.os.Build
+import com.google.android.gms.ads.MobileAds
 import pony.tothemoon.battletimer.utils.AndroidUtils
 import pony.tothemoon.battletimer.utils.NotificationUtils
 
@@ -10,6 +11,7 @@ class BattleTimerApp : Application() {
     super.onCreate()
 
     AndroidUtils.initialize(this)
+    MobileAds.initialize(this)
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
       NotificationUtils.createNotificationChannel(this)
